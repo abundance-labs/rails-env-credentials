946957f116a22a0666a96762230d64192a7cd87d
Nick Zhebrun <nikolay.zhebrun@gmail.com>
2018-06-08 14:24:43 +0300

    Prepend env credentials

--- lib/rails_env_credentials/railtie.rb
+++ lib/rails_env_credentials/railtie.rb
@@ -2,11 +2,6 @@
 
 module RailsEnvCredentials
   class Railtie < ::Rails::Railtie
-    initializer 'rails-env-credentials' do
-      is_credentials_command = Rails.const_defined?(:Command) &&
-        Rails::Command.const_defined?(:CredentialsCommand) &&
-        !Rails::Command.const_defined?(:EnvCredentialsCommand)
-      Rails::Application.prepend(CredentialsOverwrite) unless is_credentials_command
-    end
+    Rails::Application.prepend(CredentialsOverwrite)
   end
 end
